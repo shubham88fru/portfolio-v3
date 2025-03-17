83bb88ccb5cf6213b71b4117a2a179dd104119c5
shubham88fru <shubham88fru@yahoo.com>
2025-03-16 17:41:07 -0700

    hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,45 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32 md:py-48 lg:py-60">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="person behing a laptop"
+          />
+          <div className="bg-gray-950 border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">I'm available, Hire me!</div>
+          </div>
+        </div>
+
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Full stack engineering is what I do
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I'm a full stack engineer with over 5 years of experience in
+            building cloud native, and scalable backend and frontend
+            applications. Let's Connect!
+          </p>
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Checkout my Work</span>
+            <ArrowDown className="size-4" />
+          </button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+            <span>👋</span>
+            <span className="font-semibold">Let's Connect!</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
