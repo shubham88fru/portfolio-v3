1d29bd7b594203805992ce00a346586081d355ce
shubham88fru <shubham88fru@yahoo.com>
2025-03-19 17:15:26 -0700

    hero complete

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -9,6 +9,7 @@ export const HeroOrbit = ({
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div
         // className="animate-spin [animation-duration:30s]"
+        className="flex items-start justify-start"
         style={{
           height: `${size}px`,
           width: `${size}px`,
