dcfbecf190c4c8434ff365c7f07e7132dd643fb5
shubham88fru <shubham88fru@yahoo.com>
2025-03-25 18:04:51 -0700

    extract out components

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,14 @@
+import SectionHeader from "@/components/SectionHeader";
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div>
+      <SectionHeader
+        eyebrow="About Me"
+        title="A Glimpse Into My World"
+        description="Learn more about who I am, what I do, and what Inspires me."
+      />
+      {/* <Card */}
+    </div>
+  );
 };
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,7 +4,8 @@ import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import Image from "next/image";
-import grainImage from "@/assets/images/grain.jpg";
+import SectionHeader from "@/components/SectionHeader";
+import Card from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -49,27 +50,17 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">
-            Real-world Results
-          </p>
-        </div>
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
-          Featured Projects
-        </h2>
-        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-wd-md mx-auto">
-          See how I transformed concepts into engaging digital experiences.
-        </p>
+        <SectionHeader
+          eyebrow="Real-world Results"
+          title="Featured Projects"
+          description="See how I transformed concepts into engaging digital experiences."
+        />
         <div className="md:mt-20 flex flex-col mt-10 gap-20">
           {portfolioProjects.map((project, index) => (
-            <div
+            <Card
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative overflow-hidden z-0 after:z-10 after-content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+              className="px-8 pb-0 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
             >
-              <div
-                className="absolute inset-0 -z-10 opacity-10"
-                style={{ backgroundImage: `url(${grainImage.src})` }}
-              ></div>
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
                   <div className="bg-gradient-to-r from-emerald-300 gap-2 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
@@ -107,7 +98,7 @@ export const ProjectsSection = () => {
                   />
                 </div>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
       </div>
