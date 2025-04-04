5c8a2efa040438a11e32b18670f8d391ac1d3906
shubham88fru <shubham88fru@yahoo.com>
2025-04-04 14:39:16 -0700

    animate hero section

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,26 +1,50 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const HeroOrbit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  orbitDuration,
+  shouldOrbit = false,
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  orbitDuration?: string;
+  shouldOrbit?: boolean;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+}>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div
-        // className="animate-spin [animation-duration:30s]"
-        className="flex items-start justify-start"
+        className={twMerge(shouldOrbit && "animate-spin")}
         style={{
-          height: `${size}px`,
-          width: `${size}px`,
-          transform: `rotate(${rotation}deg)`,
+          animationDuration: orbitDuration,
         }}
       >
         <div
-          className=" inline-flex animate-spin [animation-duration:5s]"
-          style={{ transform: `rotate(${rotation * -1}deg)` }}
+          // className="animate-spin [animation-duration:30s]"
+          className="flex items-start justify-start"
+          style={{
+            height: `${size}px`,
+            width: `${size}px`,
+            transform: `rotate(${rotation}deg)`,
+          }}
         >
-          {children}
+          <div
+            className={twMerge(shouldSpin && "animate-spin")}
+            style={{ animationDuration: spinDuration }}
+          >
+            <div
+              className=" inline-flex"
+              style={{ transform: `rotate(${rotation * -1}deg)` }}
+            >
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -18,48 +18,66 @@ export const HeroSection = () => {
         <div className="size-[820px] animate-pulse [animation-duration:4s] hero-ring"></div>
         <div className="size-[1020px] animate-pulse [animation-duration:5s] hero-ring"></div>
         <div className="size-[1220px] animate-pulse [animation-duration:6s] hero-ring"></div>
-        <HeroOrbit size={800} rotation={-72}>
-          <StarIcon className="size-28 text-emerald-300" />
-        </HeroOrbit>
 
-        <HeroOrbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-emerald-300" />
-        </HeroOrbit>
-
-        <HeroOrbit size={590} rotation={98}>
-          <StarIcon className="size-8 text-emerald-300" />
-        </HeroOrbit>
-
-        <HeroOrbit size={430} rotation={-14}>
+        <HeroOrbit size={430} rotation={-14} shouldOrbit orbitDuration="30s">
           <SparkleIcon className="size-8 text-emerald-300/20" />
         </HeroOrbit>
 
-        <HeroOrbit size={440} rotation={79}>
+        <HeroOrbit size={440} rotation={79} shouldOrbit orbitDuration="32s">
           <SparkleIcon className="size-5 text-emerald-300/20" />
         </HeroOrbit>
 
-        <HeroOrbit size={440} rotation={79}>
-          <SparkleIcon className="size-5 text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={530} rotation={178}>
-          <SparkleIcon className="size-10 text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={710} rotation={144}>
-          <SparkleIcon className="size-14 text-emerald-300/20" />
-        </HeroOrbit>
-
-        <HeroOrbit size={720} rotation={85}>
-          <div className="size-3 bg-emerald-300/20 rounded-full" />
-        </HeroOrbit>
-
-        <HeroOrbit size={520} rotation={-41}>
+        <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration="34s">
           <div className="size-2 bg-emerald-300/20 rounded-full" />
         </HeroOrbit>
 
-        <HeroOrbit size={650} rotation={-5}>
+        <HeroOrbit size={530} rotation={178} shouldOrbit orbitDuration="36s">
+          <SparkleIcon className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+
+        <HeroOrbit
+          size={550}
+          rotation={20}
+          shouldOrbit
+          orbitDuration="38s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+
+        <HeroOrbit
+          size={590}
+          rotation={98}
+          shouldOrbit
+          orbitDuration="40s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+
+        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s">
           <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+
+        <HeroOrbit size={710} rotation={144} shouldOrbit orbitDuration="44s">
+          <SparkleIcon className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+
+        <HeroOrbit size={720} rotation={85} shouldOrbit orbitDuration="46s">
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+
+        <HeroOrbit
+          size={800}
+          rotation={-72}
+          shouldOrbit
+          orbitDuration="48s"
+          shouldSpin
+          spinDuration="7s"
+        >
+          <StarIcon className="size-28 text-emerald-300" />
         </HeroOrbit>
       </div>
       <div className="container">
@@ -70,7 +88,9 @@ export const HeroSection = () => {
             alt="person behing a laptop"
           />
           <div className="bg-gray-950 border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="animate-ping [animation-duration:2s] bg-green-500 size-2.5 rounded-full"></div>
+            <div className=" bg-green-500 size-2.5 rounded-full relative">
+              <div className="bg-green-500 absolute inset-0 rounded-full animate-ping-large"></div>
+            </div>
             <div className="text-sm font-medium">I'm available. Hire me!</div>
           </div>
         </div>
