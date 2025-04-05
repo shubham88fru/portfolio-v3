21d6db4273b50da8c71461b942a4f5a116630037
shubham88fru <shubham88fru@yahoo.com>
2025-04-05 14:39:51 -0700

    sticky effect in projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -59,7 +59,10 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project, index) => (
             <Card
               key={project.title}
-              className="px-8 pb-0 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+              className="px-8 pb-0 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${index * 40}px)`,
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
